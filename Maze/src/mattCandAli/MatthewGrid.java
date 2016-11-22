e1e852bc278fac320ab924ea78b95ac57f0d338a
ecuck <ecuck@Batcomputer.fios-router.home>
2016-11-22 01:19:36 -0500

    apparently, im coding all of connect 4 since Ali can't

--- Maze/src/mattCandAli/MatthewGrid.java
+++ Maze/src/mattCandAli/MatthewGrid.java
@@ -1,6 +1,7 @@
 package mattCandAli;
 
 import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class MatthewGrid {
@@ -13,35 +14,93 @@ public class MatthewGrid {
 	//		next open slot is filled by an o
 	//		once placed, check win conditions. horizontal, if 4 in a row, vertical, if 4 in a row, if diagonal 4 in a row
 	//		keep looping until a player wins
-	public static String[][] arr2D;
-	public static String[][] pic;
-	public static int i;
-	public static int j;
-	public static Scanner in;
+	final static int WIDTH = 7;
+	final static int HEIGHT = 6;
+	final static int LASTROW = WIDTH -1;
+	public static char[][] board = new char[WIDTH][HEIGHT];;
+	public static String[] columnKey = {"1","2","3","4","5","6","7"};
+	public static boolean gameLoop = true;
+	public static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args){
-		pic = new String[6][7];
-		for(int row = 0; row < pic.length; row++){
-			for(int col = 0; col < pic[row].length; col++){
-				pic[row][col]=" x ";
+		generatePlayingField();
+		System.out.println("Beat this game of Connect 4 to pass! Your piece will be an Select a column by choosing its corresponding index.\n 01234567");
+		printBoard(board);
+		System.out.println("You can skip this game by entering the passphrase: HANLEY");
+		if(scanner.nextLine().equals("HANLEY")){
+			gameLoop = false;
+		}
+		while(gameLoop = true){
+			playerSelect();
+			if(!checkPlayerWin()){
+	              gameLoop = false; //sets flag to false so loop is not repeated if player 1 won
+	              break; //break to skip player 2s turn if won
+	          }
+		}
+	}
+	
+	private static boolean checkPlayerWin() {
+		//check: HORIZONTAL ___
+		//		VERTICAL ___
+		//		DIAGONAL ___
+		
+		return false;
+	}
+
+	private static void playerSelect(){
+		int counter = 1;
+		System.out.println("Player's turn. Please select a valid column.");
+//		String column = scanner.getInput();
+//		for(int i = 0; i < columnKey.length; i++){
+//			if(column.equals(columnKey[i])){
+//				int col = i;
+//			}
+//		}
+		int col = scanner.nextInt();
+		boolean deciding = true;
+		while(deciding){
+			if(col > WIDTH){
+				System.out.println("That's not a valid column");
+				deciding = false;;
+			}
+			if(board[LASTROW][col] == '-'){ 
+				board[LASTROW][col] = 'o';
+				deciding = false;;
+			}
+			else if(board[LASTROW][col] == 'x' || board[LASTROW][col] == 'o'){ 
+				if(board[LASTROW - counter][col] == '-'){ 
+					board[LASTROW - counter][col] = 'o';
+					deciding = false;
+				}
+			}
+			if(counter == WIDTH){ 
+				System.out.println("Can't drop a piece here.");
+				deciding = false;;
+			}
+			counter += 1; 
+		}  
+	}
+
+
+	private static void generatePlayingField() {
+		for(int row = 0; row < board.length; row++){
+			for(int col = 0; col < board[row].length; col++){
+				board[row][col]='-';
 			}
 		}
-		in = new Scanner(System.in);
-		if(generateRandomNumberBetween(2,1) >= 2){
-			pic[pic.length-1][generateRandomNumberBetween(pic.length,0)] = " o ";
-		}
-		printPic(pic);
 	}
+
+
 
 	private static int generateRandomNumberBetween(int max,int min){
 		int randomNum = min + (int)(Math.random() * ((max - min) + 1));
 		return randomNum;
 	}
 
-	public static void printPic(String[][] pic){
-		for(int row = 0;row < pic.length;row++){
-			for(int col = 0;col < pic[row].length;col++){
-				System.out.print(pic[row][col]);
+	public static void printBoard(char[][] board2){
+		for(int row = 0;row < board2.length;row++){
+			for(int col = 0;col < board2[row].length;col++){
+				System.out.print(board2[row][col]);
 			}
 			System.out.println();
 		}
