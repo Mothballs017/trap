8e02c952f2d2e2898f59443fbcdb99316b5762a4
ecuck <ecuck@Batcomputer.fios-router.home>
2016-11-22 21:32:04 -0500

    implemented :)

--- Maze/src/caveExplorer/CaveRoom.java
+++ Maze/src/caveExplorer/CaveRoom.java
@@ -42,7 +42,7 @@ public class CaveRoom {
 				doors[EAST] == null &&
 				doors[SOUTH] == null &&
 				doors[WEST] == null){
-			directions = "\n\nThis is a room with no exit. You will die here. Sorry, you thought you would actually escape.";		
+			directions = "\n\nThis is a room with no exit.";		
 		}else{
 			for(int dir = 0; dir < doors.length; dir++){
 				if(doors[dir] != null){
--- Maze/src/caveExplorer/GameStartEvent.java
+++ Maze/src/caveExplorer/GameStartEvent.java
@@ -3,26 +3,25 @@ package caveExplorer;
 public class GameStartEvent implements Event {
 
 	public static final String[] SEQ_1 = {
-			"A spike-haired guy with orange pants flies up to you.",
-			"Have you arrived to save us from the treacherous BUU?",
-			"We need your help."};
+			"This is a room with no exit. You will die here. You thought you would actually escape.",
+			"You slam on the walls in frustration.",
+			"You: 'Isn't this supposed to be a dungeon of some sort? Where is the exit?'",
+			"Hanley: 'You're stuck in here with me! Isn't this wonderful?'"};
 	
 	public static final String[] SEQ_2 = {
-			"Great, I will be busy meditating on my super yellow version while you figure out where BUU is.",
-			"Kaaaa, maaaaay..."};
+			"Oh, okay. I'll just...wait here for another thousand years until you change your mind then."};
 	
-	public GameStartEvent() {
-		// TODO Auto-generated constructor stub
-	}
 
 	public void play() {
+		CaveExplorer.currentRoom = CaveExplorer.caves[2][2];
+		CaveExplorer.currentRoom.enter();
 		readSequence(SEQ_1);
-		CaveExplorer.print("Will you help us?");
-		while(CaveExplorer.in.nextLine().indexOf("yes")<0){
-			CaveExplorer.print("Please say yes. I beg you!");
+		CaveExplorer.print("Want to play another game?");
+		while(CaveExplorer.in.nextLine().indexOf("no")<0){
+			CaveExplorer.print("Please say yes!");
 		}
 		readSequence(SEQ_2);
-		CaveExplorer.inventory.setMap(true);
+		CaveExplorer.print("Hanley powers down leaving you in total darkness.");
 	}
 
 	public static void readSequence(String[] seq) {
